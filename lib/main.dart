78f38af8c5d1d6c105009b253b5cf9637b5ad01d
githubstar777 <muhamadbintangpramudya@gmail.com>
2023-11-07 23:22:29 +0700

    latihan6

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'menu/ketiga.dart';
+import 'menu/pertama.dart';
+import 'menu/kedua.dart';
+
+
+/// Flutter code sample for [TabBar].
+
+void main() => runApp(const TabBarApp());
+
+class TabBarApp extends StatelessWidget {
+  const TabBarApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: const TabBarExample(),
+    );
+  }
+}
+
+class TabBarExample extends StatelessWidget {
+  const TabBarExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Bintang - TIFRM21'),
+          bottom: const TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.cloud_outlined),
+              ),
+              Tab(
+                icon: Icon(Icons.beach_access_sharp),
+              ),
+              Tab(
+                icon: Icon(Icons.brightness_5_sharp),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          
+            children: const <Widget>[
+            PertamaPage(),
+            KeduaPage(),
+            KetigaPage(),
+          ],
+        ),
+      ),
+    );
+  }
+}
